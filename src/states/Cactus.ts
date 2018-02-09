d7f73e86250c03c7df9220579221303ad95f95de
Ксения Махоркина <pulpiks@yandex.ru>
2018-02-09 22:34:02 +0100

    feat/add-cactuses

--- src/states/Cactus.ts
+++ src/states/Cactus.ts
@@ -1,7 +1,28 @@
+import { CACTUS } from '../constants/constants';
+
 export default class Cactus {
+    props: any;
     constructor(props) {
+        this.props = props;
+        this.props.cactus.width = 20;
+        this.props.cactus.height = 40;
 
+        // this.game = game;
+        // this.enemy = enemy;
+        // this.person = person;
+        // this.enemies = enemies;
+        // this.enemySprite = this.enemies.create(
+        //     this.enemy.x,
+        //     this.enemy.y,
+        //     'enemy'
+        // );
+        // this.enemySprite.width = CACTUS.width;
+        // this.enemySprite.height = CACTUS.height;
+        // this.enemySprite.name = 'cactus_'+ generatorId.getIdForEnemy();
+        // this.enemySprite.anchor.set(0.5, 1);
+        // // enemySprite.body.gravity.y = 200;
+        // this.enemySprite.body.collideWorldBounds = true;
+        // this.enemySprite.body.immovable = true;
+        // this.timerChangingVelocity = Date.now();
     }
-
-
 }
